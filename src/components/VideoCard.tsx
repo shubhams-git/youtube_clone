1dff80460aa01cf018c9952c161e9ce0b8522d02
Shubham Sharma <87312848+shubhams-git@users.noreply.github.com>
2024-10-11 00:15:17 +1100

    Created a Video Card by passing props

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,20 +1,19 @@
-export function VideoCard(){
+export function VideoCard(props:any){
     return <div>
-        <img src= "/photo.jpg" className="rounded-xl"></img>
+        <img src={props.thumbnail} className="rounded-xl"></img>
         <div className="grid grid-cols-12 pt-3">
             <div className="col-span-1">
-                <img className="rounded-full w-11 h-11" src= "/logo.jpg"></img>
+                <img className="rounded-full w-11 h-11" src={props.logo}></img>
             </div>
             <div className="col-span-11 pl-1 pt-0.5">
                 <div className="font-bold text-xl">
-                    RATAN TATA PASSED AWAY | Every Indian 
-                    Salutes the Great Man | By Prashant Dhawan
+                    {props.title}
                 </div>
                 <div className="col-span-11 text-gray-400 text-lg">
-                    Study IQ IAS
+                    {props.author}
                 </div>
                 <div className="col-span-11 text-gray-400 text-lg">
-                    1M views・50 minutes ago
+                    {props.views}・{props.timestamp}
                 </div>
             </div>
             
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,12 @@ import { VideoCard } from "@/components/VideoCard";
 export default function Home() {
   return (
     <div>
-      <VideoCard />
+      <VideoCard thumbnail={"photo.jpg"}
+        logo={"logo.jpg"}
+        title={"RATAN TATA PASSED AWAY | Every Indian Salutes the Great Man | By Prashant Dhawan"}
+        author={"Study IQ IAS"}
+        views={"1M views"}
+        timestamp={"50 minutes ago"}/>
     </div>
   );
 }
