291af385ddd3177d3bc89b53e5c5670118c1f147
Shubham Sharma <87312848+shubhams-git@users.noreply.github.com>
2024-10-30 21:04:47 +1100

    Rectified the light mode flaws

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -4,10 +4,10 @@ import { SignInButton } from "./SignInButton"
 
 export const Appbar =()=>{
     return <div className="flex justify-between p-2">
-        {/* <div className="hidden dark:block text-md items-center cursor-pointer">
-            <img src="youtube-logo.svg" className="w-32 h-11 object-cover"></img>
-        </div> */}
-        <div>
+        <div className="hidden dark:block cursor-pointer">
+            <img src="youtube-logo.svg" className="w-36 h-12 object-cover cursor-pointer"></img>
+        </div>
+        <div className="block dark:hidden cursor-pointer">
             <img src="YouTube-Logo.wine.svg" className="w-44 h-12 object-cover cursor-pointer"></img>
         </div>
         <div className="flex items-center w-2/3 sm:w-2/3 md:w-3/5 lg:w-2/5">
